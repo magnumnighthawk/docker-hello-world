2206ff6e561a1ca7e8e61dac1c56ff0913c09ee2
Karthick Ramakrishnan <magnumnighthawk@gmail.com>
2019-02-27 12:34:44 +0530

    First commit
    * Docker hello world setup for a sample flash app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from redis import StrictRedis, RedisError
+import os
+import socket
+
+# Connect to Redis
+redis = StrictRedis(host='localhost', port=6379, db=0)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
